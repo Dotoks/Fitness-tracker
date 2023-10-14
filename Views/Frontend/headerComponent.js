588fcce5c27bbccdc44c577b6c3a040002dbcf0e
vickybedrova <47212770+vickybedrova@users.noreply.github.com>
2023-10-14 23:10:47 +0300

    Add files

---
+++ Views/Frontend/headerComponent.js
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <header>
+            <h1>Fitness Tracker</h1>
+            <nav>
+                <ul>
+                    <li><a href='./index.html'>Home</a></li>
+                    <li><a href='./recipes.html'>Recipes</a></li>
+                    <li><a href='./search.html'>Search</a></li>
+                    <li><a href='./about.html'>About</a></li>
+                    <li><a href='./account.html'>Account</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
