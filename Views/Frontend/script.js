c6b06e6dfca315ed6494ff0d2adfe049f7967369
vickybedrova <vikibedrova123@gmail.com>
2023-10-30 17:32:48 +0200

    centralization of signup form

--- Views/Frontend/script.js
+++ Views/Frontend/script.js
@@ -1,17 +1,44 @@
 document.getElementById('login-btn').addEventListener('click', function() {
     var slider = document.querySelector('.slider');
-    slider.classList.remove('slider-move');  // Remove the class to move the slider back
+    slider.classList.remove('slider-move');  
 
-    // Show login form and hide signup form
     document.getElementById('login-form').style.display = 'block';
     document.getElementById('signup-form').style.display = 'none';
 });
 
 document.getElementById('signup-btn').addEventListener('click', function() {
     var slider = document.querySelector('.slider');
-    slider.classList.add('slider-move');     // Add the class to move the slider
+    slider.classList.add('slider-move');    
 
-    // Show signup form and hide login form
     document.getElementById('signup-form').style.display = 'block';
     document.getElementById('login-form').style.display = 'none';
 });
+
+/*
+
+document.getElementById('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const data = {};
+    formData.forEach((value, key) => {
+        data[key] = value;
+    });
+
+    fetch('https://your-backend-endpoint/api', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json()) 
+    .then(data => {
+        console.log(data); 
+    })
+    .catch(error => {
+        console.error('There was an error!', error);
+    });
+});
+
+*/
